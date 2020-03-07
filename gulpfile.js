5b8560503c05031354e447eeb2b083ab41c03889
Виктор Малышев <ovmalisheva@gmail.com>
2020-03-07 16:38:51 +0300

    feat: install gulp

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const {src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+// Static server
+ function bs() {
+  serveSass();
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+// Clean CSS
+function mincss() {
+  return src("css/*.css")
+  .pipe(rename({suffix: ".min"}))
+  .pipe(cleanCSS())
+  .pipe(dest("./css"));
+  };
+
+ function serveSass() {
+    return src("./sass/**/*.sass", "./sass/**/*.scss")
+        .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
